be1200c9c21a0439fc64370a974e7d2f8deaed9e
keile cruz <kdcruzmaldo@uncg.edu>
2026-03-19 11:07:07 -0400

    adding in files to controller file

---
+++ backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/controller/BeautyController.java
@@ -0,0 +1,5 @@
+package com.example.GlowUpAPI.controller;
+
+public class BeautyController {
+    
+}
--- backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Service.java
+++ backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Service.java
@@ -1,5 +1,31 @@
 package com.example.GlowUpAPI.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "services")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Service {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long serviceId;
+
+    @Column(nullable = false)
+    private String name; 
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(length = 500)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "beauty_id")
+    private Beauty beauty;
 }
