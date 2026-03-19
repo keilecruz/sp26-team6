cec5f8883eaf603d42c1bd8bd3e3ec7e29c76186
keile cruz <kdcruzmaldo@uncg.edu>
2026-03-19 10:10:26 -0400

    changing in customer.java

--- backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Availability.java
+++ backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Availability.java
@@ -1,5 +1,6 @@
 package com.example.GlowUpAPI.entity;
 
+
 public class Availability {
     
 }
--- backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Customer.java
+++ backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Customer.java
@@ -1,5 +1,13 @@
 package com.example.GlowUpAPI.entity;
 
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
 public class Customer {
     
 }
