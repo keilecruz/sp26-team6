249d37a0e72c97f33a1941f838054a8c745a118a
keile cruz <kdcruzmaldo@uncg.edu>
2026-03-20 02:08:08 -0400

    edited pom.xml and availability

--- backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Availability.java
+++ backend-api/GlowUpAPI/src/main/java/com/example/GlowUpAPI/entity/Availability.java
@@ -1,6 +1,13 @@
 package com.example.GlowUpAPI.entity;
 
+import jakarta.persistence.*;
 
+@Entity
 public class Availability {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // your fields here (date, time, etc.)
 }
